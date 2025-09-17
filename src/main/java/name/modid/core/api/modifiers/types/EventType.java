0d67f20ea457ecb2372ba64eca55554b2df92a5a
Nikita Grichan <anivire.work@gmail.com>
2025-09-17 23:55:50 +0300

    refactor: update event_type namings

--- src/main/java/name/modid/core/api/modifiers/types/EventType.java
+++ src/main/java/name/modid/core/api/modifiers/types/EventType.java
@@ -2,29 +2,29 @@ package name.modid.core.api.modifiers.types;
 
 public enum EventType {
   // ON_HIT
-  LIFE_STEAL("tooltip.gemstones.event_name.lifesteal"),
-  LIGHTNING_BOLT("tooltip.gemstones.event_name.lighting_bolt"),
-  TORRENT("tooltip.gemstones.event_name.torrent"),
-  SMALL_FLAT_EXPLOSION("tooltip.gemstones.event_name.small_flat_explode"),
-  ADDITIONAL_DAMAGE("tooltip.gemstones.event_name.additional_damage"),
+  ON_HIT_LIFE_STEAL("tooltip.gemstones.event_name.lifesteal"),
+  ON_HIT_LIGHTNING_BOLT("tooltip.gemstones.event_name.lighting_bolt"),
+  ON_HIT_TORRENT("tooltip.gemstones.event_name.torrent"),
+  ON_HIT_SMALL_FLAT_EXPLOSION("tooltip.gemstones.event_name.small_flat_explode"),
+  ON_HIT_ADDITIONAL_DAMAGE("tooltip.gemstones.event_name.additional_damage"),
 
   // ON_BLOCK_BREAK
-  INCREASE_GEODES_DROP("tooltip.gemstones.event_name.increase_geodes_drop"),
-  ADDITIONAL_GOLD_DROP("tooltip.gemstones.event_name.additional_gold_drop"),
-  REGENERATE_BLOCK("tooltip.gemstones.event_name.regenerate_block"),
-  SMELTER("tooltip.gemstones.event_name.smelter"),
+  ON_BLOCK_BREAK_INCREASE_GEODES_DROP("tooltip.gemstones.event_name.increase_geodes_drop"),
+  ON_BLOCK_BREAK_ADDITIONAL_GOLD_DROP("tooltip.gemstones.event_name.additional_gold_drop"),
+  ON_BLOCK_BREAK_REGENERATE_BLOCK("tooltip.gemstones.event_name.regenerate_block"),
+  ON_BLOCK_BREAK_SMELTER("tooltip.gemstones.event_name.smelter"),
 
   // ON_DROP
-  INCREASE_MOSSY_BOX_DROP("tooltip.gemstones.event_name.increase_mossy_box_drop"),
-  COPY_ENTITY_DROP("tooltip.gemstones.event_name.copy_entity_drop"),
+  ON_DROP_INCREASE_MOSSY_BOX_DROP("tooltip.gemstones.event_name.increase_mossy_box_drop"),
+  ON_DROP_COPY_ENTITY_DROP("tooltip.gemstones.event_name.copy_entity_drop"),
 
   // PLAYER_EVENT
-  EXTRA_HEALTH("tooltip.gemstones.event_name.extra_health"),
-  HEAL("tooltip.gemstones.event_name.heal"),
-  POTION_DURATION("tooltip.gemstones.event_name.potion_duration"),
+  PLAYER_EVENT_EXTRA_HEALTH("tooltip.gemstones.event_name.extra_health"),
+  PLAYER_EVENT_HEAL("tooltip.gemstones.event_name.heal"),
+  PLAYER_EVENT_POTION_DURATION("tooltip.gemstones.event_name.potion_duration"),
 
   // WORLD_EVENT
-  INCREASE_MOB_SPAWNRATE("tooltip.gemstones.event_name.increase_mob_spawnrate");
+  WORLD_EVENT_INCREASE_MOB_SPAWNRATE("tooltip.gemstones.event_name.increase_mob_spawnrate");
 
   private final String getTranslationKey;
 
