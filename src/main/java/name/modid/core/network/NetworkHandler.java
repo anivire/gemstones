7072d94ec80bc9f26849e963de431d54ce4d3ab4
Nikita Grichan <anivire.work@gmail.com>
2026-01-13 03:35:16 +0300

    fix: airJump fall damage

--- src/main/java/name/modid/core/network/NetworkHandler.java
+++ src/main/java/name/modid/core/network/NetworkHandler.java
@@ -1,6 +1,5 @@
 package name.modid.core.network;
 
-import name.modid.core.utils.airJump.AirJumpEffects;
 import name.modid.core.utils.airJump.AirJumpLogic;
 import net.fabricmc.fabric.api.networking.v1.PayloadTypeRegistry;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
@@ -9,30 +8,17 @@ import net.minecraft.server.network.ServerPlayerEntity;
 public final class NetworkHandler {
   public static void initialize() {
     AirJumpPayload.registerCodecs();
-
     PayloadTypeRegistry.playS2C().register(OreVisionPayload.ID, OreVisionPayload.CODEC);
-
-    // ServerPlayNetworking.registerGlobalReceiver(
-    // AirJumpPayload.ID,
-    // (payload, context) -> {
-    // context.player().server.execute(() -> {
-    // var player = context.player();
-    // System.out.println("[Gemstones] AirJumpPayload received from " +
-    // player.getName().getString());
-    // });
-    // });
 
     ServerPlayNetworking.registerGlobalReceiver(
         AirJumpPayload.ID,
         (payload, context) -> {
           context.player().getServer().execute(() -> {
             ServerPlayerEntity player = context.player();
-            if (player == null)
-              return;
 
-            if (AirJumpLogic.canAirJumpServer(player)) {
+            if (player != null && AirJumpLogic.canAirJumpServer(player)) {
               AirJumpLogic.applyAirJumpImpulse(player);
-              AirJumpEffects.play(player);
+              AirJumpLogic.playEffect(player);
               AirJumpLogic.onAirJumpPerformed(player);
             }
           });
