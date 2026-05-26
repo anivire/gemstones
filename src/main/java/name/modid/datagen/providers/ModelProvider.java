7fefb9b28ec56788ecc17d2bb5e8f63e106fd58a
Nikita Grichan <anivire.work@gmail.com>
2026-05-26 18:45:59 +0300

    fix: model, balance

--- src/main/java/name/modid/datagen/providers/ModelProvider.java
+++ src/main/java/name/modid/datagen/providers/ModelProvider.java
@@ -1,6 +1,9 @@
 package name.modid.datagen.providers;
 
+import com.google.gson.JsonObject;
+
 import name.modid.Gemstones;
+import name.modid.core.content.blocks.BlocksRegistry;
 import name.modid.core.content.items.registries.ItemsRegistry;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricModelProvider;
@@ -9,6 +12,7 @@ import net.minecraft.data.client.ItemModelGenerator;
 import net.minecraft.data.client.ModelIds;
 import net.minecraft.data.client.Models;
 import net.minecraft.data.client.TextureMap;
+import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.registry.Registries;
 import net.minecraft.util.Identifier;
@@ -25,6 +29,16 @@ public class ModelProvider extends FabricModelProvider {
   @Override
   public void generateItemModels(ItemModelGenerator itemModelGenerator) {
     for (Item item : ItemsRegistry.getAllItems()) {
+      if (item instanceof BlockItem) {
+        if (item == BlocksRegistry.JEWELRY_TABLE.asItem()) {
+          JsonObject model = new JsonObject();
+          model.addProperty("parent", "gemstones:block/jewelry_table");
+          itemModelGenerator.writer.accept(ModelIds.getItemModelId(item), () -> model);
+        }
+
+        continue;
+      }
+
       String path = Registries.ITEM.getId(item).getPath();
       String baseName = path.replaceAll("_(crude|polished|flawless|radiant|unusual)$", "");
 
