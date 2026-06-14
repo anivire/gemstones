16521535c814b5b875b2b7a5f276321836fff4ad
Nikita Grichan <anivire.work@gmail.com>
2026-06-14 17:20:42 +0300

    fix(neoforge): duplicated modname below quality

--- neoforge/src/main/java/name/modid/neoforge/client/GemstonesNeoForgeClientEvents.java
+++ neoforge/src/main/java/name/modid/neoforge/client/GemstonesNeoForgeClientEvents.java
@@ -4,47 +4,37 @@ import com.mojang.datafixers.util.Either;
 import java.util.List;
 
 import name.modid.Gemstones;
+import name.modid.core.api.tooltips.QualityTooltipData;
 import net.minecraft.registry.Registries;
 import net.minecraft.text.StringVisitable;
 import net.minecraft.text.Text;
+import net.minecraft.item.tooltip.TooltipData;
 import net.neoforged.neoforge.client.event.RenderTooltipEvent;
 import net.neoforged.neoforge.event.entity.player.ItemTooltipEvent;
 
 public final class GemstonesNeoForgeClientEvents {
   private static final String MOD_NAME_TRANSLATION_KEY = "item_group.gemstones";
+  private static final int QUALITY_TOOLTIP_INDEX = 1;
 
   private GemstonesNeoForgeClientEvents() {
   }
 
   public static void onItemTooltip(ItemTooltipEvent event) {
     if (Registries.ITEM.getId(event.getItemStack().getItem()).getNamespace().equals(Gemstones.MOD_ID)) {
-      removeDuplicateModNameLines(event.getToolTip());
+      removeNonFooterModNameLines(event.getToolTip());
     }
   }
 
   public static void onGatherTooltipComponents(RenderTooltipEvent.GatherComponents event) {
     if (Registries.ITEM.getId(event.getItemStack().getItem()).getNamespace().equals(Gemstones.MOD_ID)) {
-      removeDuplicateModNameElements(event.getTooltipElements());
+      moveQualityTooltipElement(event.getTooltipElements());
+      removeNonFooterModNameElements(event.getTooltipElements());
     }
   }
 
-  private static void removeDuplicateModNameLines(List<Text> tooltip) {
-    int lastModNameIndex = -1;
-    int modNameCount = 0;
-
-    for (int i = 0; i < tooltip.size(); i++) {
-      if (isModNameLine(tooltip.get(i))) {
-        lastModNameIndex = i;
-        modNameCount++;
-      }
-    }
-
-    if (modNameCount < 2) {
-      return;
-    }
-
+  private static void removeNonFooterModNameLines(List<Text> tooltip) {
     for (int i = tooltip.size() - 1; i >= 0; i--) {
-      if (i != lastModNameIndex && isModNameLine(tooltip.get(i))) {
+      if (i != tooltip.size() - 1 && isModNameLine(tooltip.get(i))) {
         tooltip.remove(i);
       }
     }
@@ -54,25 +44,26 @@ public final class GemstonesNeoForgeClientEvents {
     return line.getString().equals(Text.translatable(MOD_NAME_TRANSLATION_KEY).getString());
   }
 
-  private static void removeDuplicateModNameElements(List<? extends Either<StringVisitable, ?>> tooltipElements) {
-    int lastModNameIndex = -1;
-    int modNameCount = 0;
+  private static void moveQualityTooltipElement(List<Either<StringVisitable, TooltipData>> tooltipElements) {
+    QualityTooltipData qualityTooltipData = null;
 
-    for (int i = 0; i < tooltipElements.size(); i++) {
-      Either<StringVisitable, ?> element = tooltipElements.get(i);
-      if (element.left().isPresent() && isModNameElement(element.left().get())) {
-        lastModNameIndex = i;
-        modNameCount++;
+    for (int i = tooltipElements.size() - 1; i >= 0; i--) {
+      Either<StringVisitable, TooltipData> element = tooltipElements.get(i);
+      if (element.right().isPresent() && element.right().get() instanceof QualityTooltipData data) {
+        qualityTooltipData = data;
+        tooltipElements.remove(i);
       }
     }
 
-    if (modNameCount < 2) {
-      return;
+    if (qualityTooltipData != null) {
+      tooltipElements.add(Math.min(QUALITY_TOOLTIP_INDEX, tooltipElements.size()), Either.right(qualityTooltipData));
     }
+  }
 
+  private static void removeNonFooterModNameElements(List<Either<StringVisitable, TooltipData>> tooltipElements) {
     for (int i = tooltipElements.size() - 1; i >= 0; i--) {
-      Either<StringVisitable, ?> element = tooltipElements.get(i);
-      if (i != lastModNameIndex && element.left().isPresent() && isModNameElement(element.left().get())) {
+      Either<StringVisitable, TooltipData> element = tooltipElements.get(i);
+      if (i != tooltipElements.size() - 1 && element.left().isPresent() && isModNameElement(element.left().get())) {
         tooltipElements.remove(i);
       }
     }
